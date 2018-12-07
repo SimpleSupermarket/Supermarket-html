6cd35a25e40d02f8ac03cef2e854c0dd85524bcd
董文强 <826112946@qq.com>
2018-12-07 16:58:06 +0800

    a

--- src/components/page/user/add.vue
+++ src/components/page/user/add.vue
@@ -3,25 +3,25 @@
         <el-form :model="formData" :rules="rules"
                  ref="form" label-width="100px"
                  style="width: 33%">
-            <el-form-item label="用户名" prop="code"  >
-                <el-input v-model="formData.userId"></el-input>
+            <el-form-item label="用户名" prop="code">
+                <el-input v-model="formData.code"></el-input>
             </el-form-item>
             <el-form-item label="姓名" prop="name">
-                <el-input v-model="formData.userName"></el-input>
+                <el-input v-model="formData.name"></el-input>
             </el-form-item>
             <el-form-item label="用户密码" prop="password">
-                <el-input v-model="formData.password" ></el-input>
+                <el-input v-model="formData.password"></el-input>
             </el-form-item>
             <el-form-item label="确认密码" prop="rePassword">
                 <el-input v-model="formData.rePassword"></el-input>
             </el-form-item>
             <el-form-item label="用户性别">
-                <el-select v-model="formData.gender" placeholder="供应商编号">
-                    <el-option label="男"
-                               value="2" key="man"></el-option>
-                    <el-option label="女"
-                               value="1" key="woman"></el-option>
-                </el-select>
+                <el-radio-group v-model="formData.gender">
+                    <el-radio-button label="男"
+                                     value="2"></el-radio-button>
+                    <el-radio-button label="女"
+                                     value="1"></el-radio-button>
+                </el-radio-group>
             </el-form-item>
             <el-form-item label="出生日期" prop="birthday">
                 <el-input v-model="formData.birthday"></el-input>
@@ -34,7 +34,8 @@
             </el-form-item>
             <el-form-item label="用户类别" prop="roleId">
                 <el-radio-group v-model="formData.roleId">
-                    <el-radio-button v-for="(role,index) in roles" :value="role.id" :label="role.name" :key="index"></el-radio-button>
+                    <el-radio-button v-for="(role,index) in roles" :value="role.id" :label="role.name"
+                                     :key="index"></el-radio-button>
                 </el-radio-group>
             </el-form-item>
             <el-form-item>
@@ -50,7 +51,7 @@
         name: "goods_add",
         data() {
             return {
-                id:0,
+                id: 0,
                 rules: {
                     userId: [
                         {required: true, message: '用户编码不能为空'}
@@ -59,21 +60,21 @@
                 formData: {}
             }
         },
-        created(){
+        created() {
         },
         activated() {
-            if(this.$route.path.indexOf("/update")>1){
+            if (this.$route.path.indexOf("/update") > 1) {
                 this.$route.query.id = this.id;
-            }else{
+            } else {
                 this.id = 0;
             }
             this.init();
         },
         methods: {
-            init(){
+            init() {
                 //TODO初始化
-                if(this.id != 0){
-                    this.$axios.get("").then(res=>{
+                if (this.id != 0) {
+                    this.$axios.get("").then(res => {
                         this.formData = res.data;
                     });
                 }
